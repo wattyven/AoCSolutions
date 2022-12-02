5a6d156519fda1858cf78cf9f0d5781c387a862f
Watty <66798673+wattyven@users.noreply.github.com>
2022-12-02 13:28:09 -0800

    Update Day 02.py
    cleaned up my if statements a little bit

--- 2022/Day 02/Day 02.py
+++ 2022/Day 02/Day 02.py
@@ -1,7 +1,5 @@
 filepath = 'Day 02 input.txt'
 
-# hardcoded everything
-
 def openFile(filepath):
     '''reads the raw file'''
     with open(filepath, 'r') as f:
@@ -10,53 +8,58 @@ def openFile(filepath):
     return(data)
 
 def scoreRound(data):
-    '''scores each round in the input file and adds it to the total score'''
+    '''scores each round in the input file'''
     score = 0
     for i in data:
-        if i[0] == 'A' and i[1] == 'X':
-            score += 4
-        elif i[0] == 'A' and i[1] == 'Y':
-            score += 8
-        elif i[0] == 'A' and i[1] == 'Z':
-            score += 3
-        elif i[0] == 'B' and i[1] == 'X':
-            score += 1
-        elif i[0] == 'B' and i[1] == 'Y':
-            score += 5
-        elif i[0] == 'B' and i[1] == 'Z':
-            score += 9
-        elif i[0] == 'C' and i[1] == 'X':
-            score += 7
-        elif i[0] == 'C' and i[1] == 'Y':
-            score += 2
-        elif i[0] == 'C' and i[1] == 'Z':
-            score += 6
-        else:
-            score += 0
+        if i[0] == 'A':
+            if i[1] == 'X':
+                score += 4
+            elif i[1] == 'Y':
+                score += 8
+            elif i[1] == 'Z':
+                score += 3
+        elif i[0] == 'B':
+            if i[1] == 'X':
+                score += 1
+            elif i[1] == 'Y':
+                score += 5
+            elif i[1] == 'Z':   
+                score += 9
+        elif i[0] == 'C':
+            if i[1] == 'X':
+                score += 7
+            elif i[1] == 'Y':
+                score += 2
+            elif i[1] == 'Z':
+                score += 6
+
     return(score)
 
 def scoreRound2(data):
-    '''takes computer's choice and player's instruction (to win or draw or lose) and returns score'''
+    '''scores each round in the input file according to the new rules'''
     score = 0
     for i in data:
-        if i[0] == 'A' and i[1] == 'X':
-            score += 3
-        elif i[0] == 'A' and i[1] == 'Y':
-            score += 4
-        elif i[0] == 'A' and i[1] == 'Z':
-            score += 8
-        elif i[0] == 'B' and i[1] == 'X':
-            score += 1
-        elif i[0] == 'B' and i[1] == 'Y':
-            score += 5
-        elif i[0] == 'B' and i[1] == 'Z':   
-            score += 9
-        elif i[0] == 'C' and i[1] == 'X':
-            score += 2
-        elif i[0] == 'C' and i[1] == 'Y':
-            score += 6
-        elif i[0] == 'C' and i[1] == 'Z':
-            score += 7
+        if i[0] == 'A':
+            if i[1] == 'X':
+                score += 3
+            elif i[1] == 'Y':
+                score += 4
+            elif i[1] == 'Z':
+                score += 8
+        elif i[0] == 'B':
+            if i[1] == 'X':
+                score += 1
+            elif i[1] == 'Y':
+                score += 5
+            elif i[1] == 'Z':   
+                score += 9
+        elif i[0] == 'C':
+            if i[1] == 'X':
+                score += 2
+            elif i[1] == 'Y':
+                score += 6
+            elif i[1] == 'Z':
+                score += 7
     return(score)
 
 if __name__ == '__main__':
